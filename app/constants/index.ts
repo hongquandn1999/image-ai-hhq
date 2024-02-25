2f8eb7f2f2f24818d6f70579b667dc5c64925edb
Hong Quan Huynh <hhq851999@gmail.com>
2024-02-25 18:01:28 +0700

    sidebar

---
+++ app/constants/index.ts
@@ -0,0 +1,30 @@
+export const navLinks = [
+  { route: '/', label: 'Home', icon: '/assets/icons/home.svg' },
+  {
+    route: '/transformations/add/restore',
+    label: 'Image Restore',
+    icon: '/assets/icons/image.svg',
+  },
+  {
+    route: '/transformations/add/fill',
+    label: 'Generative Fill',
+    icon: '/assets/icons/stars.svg',
+  },
+  {
+    route: '/transformations/add/remove',
+    label: 'Object Remove',
+    icon: '/assets/icons/scan.svg',
+  },
+  {
+    route: '/transformations/add/recolor',
+    label: 'Object Recolor',
+    icon: '/assets/icons/filter.svg',
+  },
+  {
+    route: '/transformations/add/removeBackground',
+    label: 'Remove Background',
+    icon: '/assets/icons/camera.svg',
+  },
+  { route: '/profile', label: 'Profile', icon: '/assets/icons/profile.svg' },
+  { route: '/credits', label: 'Buy Credits', icon: '/assets/icons/bag.svg' },
+];
