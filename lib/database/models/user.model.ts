a0ddc485ba9b9d8120ef2c49cd38b1c568e28086
Hong Quan Huynh <hhq851999@gmail.com>
2024-03-08 19:41:51 +0700

    Setup Database

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
+
+const User = mongoose.models?.User || mongoose.model('User', UserSchema);
+
+export default User;
